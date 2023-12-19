dc61becb682bf1efaa2432d99352804959649159
s308251 <148890933+s308251@users.noreply.github.com>
2023-12-19 13:06:15 +0100

    Add files via upload

---
+++ H-R-esame.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget https://raw.githubcontent.com/MilenaValentini/TRM_Dati/main/Nemo_6670.dat
+if [ $? -eq 0 ]
+    then 
+        echo OK
+    else
+        echo FAULT
+fi
+ls -l Nemo_6670.dat
+
+export DATA_FILE_PATH='pwd'/Nemo_6670.dat
+echo $DATA_FILE_PATH
+
+
+
+python3 H-R-esame.py "$DATA_FILE_PATH"
