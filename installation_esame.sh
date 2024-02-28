7bb6365a7b6f1ae3896efbcdc355c9d7aa961364
s308251 <148890933+s308251@users.noreply.github.com>
2024-02-28 17:45:48 +0100

    Add files via upload

---
+++ installation_esame.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#creare directory dove copiare applicazione
+mkdir -p dir_esame
+
+#copiare lo start_script bash e il file python in opportuni path
+cp download_dati.sh  dir_esame/
+cp Python_esame_Papini.py  dir_esame/
+
+
+
+# attribuire i permessi d'esecuzione corretti
+
+chmod a+x installation_esame.sh
+chmod a+rx download_dati.sh
+chmod a+rx Python_esame_Papini.py
+
+#lancio script di esecuzione
+./download_dati.sh
+
+
+
+
