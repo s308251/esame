2e1967a51598961eee88a6864e6ce3b6fdca31df
s308251 <148890933+s308251@users.noreply.github.com>
2024-02-28 23:12:36 +0100

    download_dati.sh

--- download_dati.sh
+++ download_dati.sh
@@ -9,9 +9,11 @@ if [ $? -eq 0 ]
 fi
 ls -l Nemo_6670.dat
 
-export DATA_FILE_PATH='pwd'//Nemo_6670.dat
+SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
+DATA_FILE_PATH="$SCRIPT_PATH/Nemo_6670.dat"
+export DATA_FILE_PATH
 echo $DATA_FILE_PATH
 
 
-
 python3 python_esame_Papini.py "$DATA_FILE_PATH"
+
